0ece2d531bea973f2bf290e3edcdd81bb223d5ab
KangWon LEE VISSIM user <kangwon.lee@kpu.ac.kr>
2016-09-12 22:19:29 +0900

    added commandline arguments to main

--- pybindiff.py
+++ pybindiff.py
@@ -2,9 +2,15 @@
 import os
 
 
-def main():
+def main(full_path0, full_path1):
     pass
 
 
 if __name__ == '__main__':
-    main()
+    import sys
+
+    if 3 == len(sys.argv):
+        # number of arguments
+        filename0, filename1 = sys.argv[1], sys.argv[2],
+
+        main(filename0, filename1)
